64d84ca678a2258ad1f1c5321f500f2841fff0e6
coosoti <charlesosoti@gmail.com>
2021-02-03 11:16:28 +0000

    update print a sentence using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,16 @@
 #include  <stdio.h>
 
+/**
+ *main - Entry point
+ *
+ *Return: 0 on success
+ *
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+
+
+	return (0);
 }
