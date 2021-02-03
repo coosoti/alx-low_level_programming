7baa7876a1625e4866f4f6b224df2773e663c49f
coosoti <charlesosoti@gmail.com>
2021-02-03 10:54:51 +0000

    update print a sentence using puts() library

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * return - 0 on success
+ *
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+
+
+	return (0);
 }
