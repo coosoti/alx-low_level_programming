3544b8968e7c349053a2699d3e8071108d80cb97
coosoti <charlesosoti@gmail.com>
2021-02-04 18:01:28 +0000

    code to print single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+
+	return (0);
+
+}
