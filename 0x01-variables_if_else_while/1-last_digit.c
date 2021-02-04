0d7d44d4a44a26f757f0195f4cee6cda2a7f6f6b
coosoti <charlesosoti@gmail.com>
2021-02-04 06:31:57 +0000

    c program to compare the last digit of random number with chosen number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,42 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ *
+ */
+
 int main(void)
 {
 	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
+	/*to get the last digit of n, we use modulo % to find rem, x of n%10 */
+	x = n % 10;
+
+	if (x > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, x);
+
+	}
+
+	else if (x == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, x);
+
+	}
+
+	else if (x < 6 && x != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, x);
+
+	}
+
 	return (0);
 }
