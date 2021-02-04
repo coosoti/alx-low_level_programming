d1c5506fb5bd1c5649c1d0907392f598dd011ed9
coosoti <charlesosoti@gmail.com>
2021-02-04 06:00:25 +0000

    c if..else...statement to print whether a random number is + or - or 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,14 +1,40 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+#include <stdio.h>
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success
+ */
+
+
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
+	if (n > 0)
+	{
+
+		printf("%d is positive\n", n);
+
+	}
+
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+
+	}
+
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+
+	}
+
+
 	return (0);
 }
