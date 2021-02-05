a29b72f769c582e3b3518fd4fd65baa149a54c54
coosoti <charlesosoti@gmail.com>
2021-02-05 13:11:21 +0300

    code to write to stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * @void - no params
+ *
+ * Return: 1 stderr
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
