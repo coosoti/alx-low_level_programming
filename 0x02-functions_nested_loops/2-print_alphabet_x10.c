2d81d9897a5d5f188f544c76ee5a89302935262c
coosoti <charlesosoti@gmail.com>
2021-02-08 10:52:08 +0300

    function to print alphabets 10 times in lowercases

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_10x - entry level
+ *
+ * Return: Always 0
+ */
+
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter = 'a';
+
+	for (i = 0; i < 10; i++)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+			_putchar(letter);
+		}
+		_putchar('\n');
+	}
+
+}
