7b8ddd108e79dc3aa56031046ab21fb1207b44c8
coosoti <charlesosoti@gmail.com>
2021-02-08 16:01:11 +0300

    func that add 2 ints and return result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - func to add two integers
+ * @a: first int
+ * @b: second int
+ * Return: results of the addition
+ *
+ */
+
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}
