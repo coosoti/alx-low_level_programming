f1d442f11a62f0304d458d8dc271ff263256c709
coosoti <charlesosoti@gmail.com>
2021-02-11 08:21:00 +0300

    func to multiply two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * mul - a func to multiply two integers
+ *
+ * @a: first int
+ * @b: second int
+ *
+ * Return: result
+ *
+ */
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+}
