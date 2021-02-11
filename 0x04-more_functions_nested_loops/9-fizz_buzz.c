480ecb67769541bc9750524e38e68c14b8f4de85
coosoti <charlesosoti@gmail.com>
2021-02-11 21:45:11 +0300

    update fizzbuzz function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -17,15 +17,15 @@ int main(void)
 	for (i = 1; i <= 100; i++)
 	{
 
-		if (i % 3 == 0)
+		if ((i % 3) == 0 && (i % 5) != 0)
 
 			printf("Fizz");
 
-		else if (i % 5 == 0)
+		else if ((i % 3) != 0 && (i % 5) == 0)
 
 			printf("Buzz");
 
-		else if (i % 3 == 0 && i % 5 == 0)
+		else if ((i % 3) == 0 && (i % 5) == 0)
 
 			printf("FizzBuzz");
 
