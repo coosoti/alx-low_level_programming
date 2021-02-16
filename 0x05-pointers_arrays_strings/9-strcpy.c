e8cc6cbfbeb31214319281232cd37bd8d1214cea
coosoti <charlesosoti@gmail.com>
2021-02-16 13:13:45 +0300

    func to copy str from source to destination

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -14,13 +14,17 @@ char *_strcpy(char *dest, char *src)
 {
 
 	int i = 0;
+	int len = 0;
 
-	while (src[i])
-	{
+	while (src[i++])
+
+		len++;
+
+	for (i = 0; i <= len; i++)
+
 
 		dest[i] = src[i];
-		i++;
-	}
+
 
 	return (dest);
 }
