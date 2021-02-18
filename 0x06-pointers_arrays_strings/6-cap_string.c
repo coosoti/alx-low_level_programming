6aeda0b0868b60bb693973fcbcb4db567ebc1428
coosoti <charlesosoti@gmail.com>
2021-02-18 18:42:10 +0300

    a funct to capitalize each first letter in a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: pointer to string
+ * Return: the pointer to the string
+ */
+
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+
+		if (s[i - 1] == ' ' ||
+		        s[i - 1] == '\t' ||
+		        s[i - 1] == '\n' ||
+		    s[i - 1] == ',' ||
+		    s[i - 1] == ';' ||
+		        s[i - 1] == '.' ||
+		        s[i - 1] == '!' ||
+		        s[i - 1] == '?' ||
+		        s[i - 1] == '"' ||
+		    s[i - 1] == '(' ||
+		    s[i - 1] == ')' ||
+		    s[i - 1] == '{' ||
+		    s[i - 1] == '}' ||
+		    i == 0)
+			s[i] -= 32;
+
+		i++;
+	}
+
+	return (s);
+}
