781a341abc0f98d2237769fdb7cf8c6b331b1ac8
coosoti <charlesosoti@gmail.com>
2021-02-18 19:06:55 +0300

    a function to implement rot13

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: pointer to string
+ * Return: the pointer to the string
+ */
+
+char *rot13(char *str)
+{
+	int i, j;
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(str + i); i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(str + i))
+			{
+				*(str + i) = b[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
