0814d60cce0b3d33b8ece4e6383a6ab7e86de810
coosoti <charlesosoti@gmail.com>
2021-02-18 04:22:46 +0300

    function that concatenates two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _strcat - appends src to the dest string
+ * @dest: string to append by src
+ * @src: string to append to dest
+ *
+ * Return: address of dest
+ */
+
+char *_strcat(char *dest, const char *src)
+{
+	int i = 0;
+	int dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i]; i++)
+		dest[dest_len++] = src[i];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+
+#include <stdlib.h>
+
+int _putchar(char c);
+char *_strcat(char *dest, char *src);
+
+#endif
