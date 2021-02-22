109569c95ce67cf13f27dd60b81968ac83d67ef7
coosoti <charlesosoti@gmail.com>
2021-02-22 10:08:55 +0300

    func to n bytes from memory area src to memory area dest

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - Copies @n bytes from the memory area pointed
+ *           to by @src into that pointed to by @dest.
+ * @dest: A pointer to the memory area to copy @src into.
+ * @src: The source buffer to copy characters from.
+ * @n: The number of bytes to copy from @src.
+ *
+ * Return: A pointer to the destination buffer @dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+
+	}
+
+	return (dest);
+
+}
