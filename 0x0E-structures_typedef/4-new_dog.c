9c89d2a1480569dbc3c79054a243ad48b9d25ff5
coosoti <charlesosoti@gmail.com>
2021-03-08 16:43:07 +0300

    update function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,31 +18,24 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
-
 	n_dog = malloc(sizeof(dog_t));
-
 	/* New dog's name */
 	if (n_dog == NULL)
 		return (NULL);
-
 	for (n = 0; name[n]; n++)
 		;
 	n++;
 
 	n_dog->name = malloc(n * sizeof(char));
-
 	if (n_dog->name == NULL)
 	{
 		free(n_dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < n; i++)
 		n_dog->name[i] = name[i];
-
 	/* New dog age */
 	n_dog->age = age;
-
 	for (o = 0; owner[o]; o++)
 		;
 	o++;
@@ -58,5 +51,4 @@ dog_t *new_dog(char *name, float age, char *owner)
 	for (i = 0; i < o; i++)
 		n_dog->owner[i] = owner[i];
 	return (n_dog);
-
 }
