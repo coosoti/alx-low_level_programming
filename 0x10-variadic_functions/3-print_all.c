e4947c793f5f5dc891701b2c02003256d8a92713
coosoti <charlesosoti@gmail.com>
2021-03-11 19:12:55 +0300

    function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,97 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * _printc- a function to print a char
+ * @c: char to print
+ * Return: void
+ */
+
+void _printc(va_list c)
+{
+
+	printf("%c", va_arg(c, int));
+}
+
+/**
+ * _prints - a function to print a string
+ * @s: string to print
+ * Return: void
+ */
+
+void _prints(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+ * _printi - a function to print an int
+ * @i: int to print
+ *
+ * Return: void
+ */
+void _printi(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+ * _printf - a function that prints a float
+ * @f: float to print
+ *
+ * Return: void
+ */
+void _printf(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
+ * print_all - prints anything
+ * @format: list of argument types passed to the function
+ *
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	dt_t types[] = {
+		{"c", _printc},
+		{"i", _printi},
+		{"f", _printf},
+		{"s", _prints},
+		{NULL, NULL}
+	};
+
+	int i, j = 0;
+	va_list a_list;
+	char *separator = "";
+
+	va_start(a_list, format);
+
+	while (format != NULL && format[i])
+	{
+		j = 0;
+		while (types[j].type)
+		{
+			if (format[i] == *types[j].type)
+			{
+				printf("%s", separator);
+				types[j].func(a_list);
+				separator = ", ";
+
+			}
+			j++;
+
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(a_list);
+
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,5 +6,17 @@
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+/**
+ * struct print - print type with corresponding print function
+ * @t: print type
+ * @f: print function
+ */
+typedef struct print
+{
+	char *type;
+	void (*func)(va_list);
+} dt_t;
 
 #endif /* _VARIADIC_FUNCTIONS_H */
